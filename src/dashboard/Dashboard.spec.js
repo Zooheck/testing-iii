992518c7c9f101054abcfe4d4d0fedeb12d04886
Zooheck <davidscottflack@gmail.com>
2019-03-06 16:57:20 -0600

    Gate button fires

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,26 @@
-// Test away
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
+import renderer from "react-test-renderer";
+import "react-testing-library/cleanup-after-each";
+
+import Dashboard from "./Dashboard";
+import Controls from "../controls/Controls";
+import Display from "../display/Display";
+
+let state = {
+  locked: false,
+  closed: false
+};
+
+const { locked, closed } = state;
+
+describe("app functionality", () => {
+  it("gate button should fire", () => {
+    const gateMock = jest.fn();
+    const { getByTestId } = render(<Controls toggleClosed={gateMock} />);
+    const gateButton = getByTestId("gate-button");
+    fireEvent.click(gateButton);
+    expect(gateMock).toHaveBeenCalled();
+  });
+});
