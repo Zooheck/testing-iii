9684fa1769c0210a862c790a022a3201ad210d72
Zooheck <davidscottflack@gmail.com>
2019-03-06 16:10:43 -0600

    Test default display values

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
-// Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
+import renderer from "react-test-renderer";
+import "react-testing-library/cleanup-after-each";
+
+import Display from "./Display";
+
+describe("display component", () => {
+  it("should default to displaying both open/unlocked", () => {
+    const { getByText } = render(<Display />);
+
+    const lockDisplay = getByText(/unlocked/i);
+    const openDisplay = getByText(/open/i);
+
+    expect(lockDisplay.textContent).toBe("Unlocked");
+    expect(openDisplay.textContent).toBe("Open");
+  });
+});
