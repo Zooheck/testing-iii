d53cb068a90c111419bba9dbeacb63029cf37faa
Zooheck <davidscottflack@gmail.com>
2019-03-06 18:01:49 -0600

    Gate button enabled and disabled at proper instances

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,22 @@
-// Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
+import renderer from "react-test-renderer";
+import "react-testing-library/cleanup-after-each";
+
+import Controls from "./Controls";
+
+describe("controls being disabled", () => {
+  it("gate button should be disabled when closed and locked", () => {
+    const { getByTestId } = render(<Controls locked={true} closed={true} />);
+    const gateButton = getByTestId("gate-button");
+
+    expect(gateButton).toBeDisabled();
+  });
+  it("gate button should be enabled when unlocked", () => {
+    const { getByTestId } = render(<Controls locked={false} closed={true} />);
+    const gateButton = getByTestId("gate-button");
+
+    expect(gateButton).not.toBeDisabled();
+  });
+});
